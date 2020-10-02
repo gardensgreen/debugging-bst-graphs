bb9e526f16313e226c32450fbf301b7044514b38
carlos molero <cmolero130@gmail.com>
2020-10-02 14:43:28 -0400

    Remove Zone Identifiers

--- hints/graphs/numRegions.js
+++ hints/graphs/numRegions.js
@@ -4,9 +4,9 @@ function numRegions(graph) {
     for (key in graph) {
         // missing a line in this general area
         regions += 1;
-        
+
         // and one in this general area
         set.add(...graph[key]);
-    };
+    }
     return regions;
 }
